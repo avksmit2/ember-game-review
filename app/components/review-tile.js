ae81091e722a77bd39dc074d1aebe175bf635111
Angela Smith <avksmit2@gmail.com>
2016-11-04 10:30:12 -0700

    add a review-tile to the game-detail component.

--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  reviewHeading: Ember.computed('review.user', 'review.timestamp', function() {
+    return 'posted by: ' + this.get('review.user') + ' ' moment(this.get('review.timestamp').fromNow());
+  })
 });

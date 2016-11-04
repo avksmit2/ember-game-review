86fa913fad99583bad5483dbaadeccae65cb3bcc
Angela Smith <avksmit2@gmail.com>
2016-11-04 08:14:55 -0700

    add model details for game and reviews.

--- app/models/game.js
+++ app/models/game.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr('string'),
+  releaseDate: DS.attr('date'),
+  details: DS.attr('string'),
+  reviews: DS.hasMany('review', {async: true})
 });

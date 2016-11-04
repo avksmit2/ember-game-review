d54a516565162f096464e7f8b7b2435817e12b31
Angela Smith <avksmit2@gmail.com>
2016-11-04 09:57:36 -0700

    create update form and functionality.

--- app/components/update-game.js
+++ app/components/update-game.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
+  isShowingModal: false,
+  actions: {
+    updateGame(game) {
+      var params = {
+        user: this.get('user'),
+        gameName: this.get('gameName'),
+        image: this.get('image'),
+        releaseDate: this.get('releaseDate'),
+        timestamp: moment().valueOf(),
+        details: this.get('details'),
+        summary: this.get('summary')
+      };
+      this.set('isShowingModal', false);
+      this.sendAction('updateGame', game, params);
+    },
+    toggleModal: function() {
+      this.toggleProperty('isShowingModal');
+    }
+  }
 });

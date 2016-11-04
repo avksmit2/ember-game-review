3f8b49857267e086e9c17e0f021c0078304c6f77
Angela Smith <avksmit2@gmail.com>
2016-11-04 08:56:26 -0700

    add the game tile and call it from the index template.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('game', {path: '/game/:game_id'});
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('game');
+  },
+  actions: {
+    addGame(params) {
+      var newGame = this.store.createRecord('game', params);
+      newGame.save();
+      this.transitionTo('index');
+    }
+  }
 });
